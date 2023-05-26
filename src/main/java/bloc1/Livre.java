23584dc4cd5489b9b151857f67d90977ab3d1ee0
Cindy Dallaire <cindy.dallaire@timesphere.com>
2023-05-26 08:05:43 -0400

    t

--- src/main/java/bloc1/Livre.java
+++ src/main/java/bloc1/Livre.java
@@ -6,4 +6,10 @@ public class Livre {
     public String Auteur;
     public int NombrePages;
     public String Editeur;
+
+    public void tada()
+    {
+        int t = 0;
+        t += 1;
+    }
 }
