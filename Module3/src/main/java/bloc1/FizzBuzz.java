6eb2b175429025469e37c8a1b6496d30ad80e61d
Cindy Dallaire <cindy.dallaire@timesphere.com>
2023-05-29 09:33:40 -0400

    test code coverage

--- Module3/src/main/java/bloc1/FizzBuzz.java
+++ Module3/src/main/java/bloc1/FizzBuzz.java
@@ -1,5 +1,8 @@
 package bloc1;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FizzBuzz {
 
     public static String FIZZ = "Fizz";
@@ -15,6 +18,9 @@ public class FizzBuzz {
 //            return FIZZ;
 //        return String.valueOf(value);
 
+        List<Integer> t = List.of(4,5,6);
+
+        List<Integer> s = t.stream().collect(Collectors.toList());
 
         if(value % 5 != 0 && value % 3 != 0)
             return String.valueOf(value);
